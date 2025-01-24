6661bf378be000f13a89e0fd9807fcf5fd4b3093
dmmdeveloper <zongdostm@gmail.com>
2025-01-23 23:15:34 -0800

    set Profile-1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,7 +7,7 @@ const {profile} =useAppContext()
 
   return (
 <>
-<div className="h-screen w-full bg-slate-600 text-white text-5xl flex justify-center items-center flex-col">
+<div className="h-screen w-full bg-slate-600 text-white text-2xl md:text-5xl flex justify-center items-center flex-col">
     <h1>{profile.name}</h1>
     <h1>{profile.email}</h1>
 
